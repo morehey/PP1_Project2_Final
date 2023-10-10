a6d00bfff43f05331af8d277ece0bf3e25231204
junghwan <morehey1536@gmail.com>
2023-10-10 23:19:22 +0900

    test

--- form_ok.php
+++ form_ok.php
@@ -1,21 +1,40 @@
 <?php
 
 // 1. Process incoming form data
-$product_data = [
-    'name' => $_POST['name'],
-    'type' => $_POST['type'],
-    'price' => $_POST['price'],
-    'quantity' => $_POST['quantity'],
-    'status' => $_POST['status']
-];
+#$product_data = [
+#    'name' => $_POST['name'],
+#    'type' => $_POST['type'],
+#    'price' => $_POST['price'],
+#    'quantity' => $_POST['quantity'],
+#    'status' => $_POST['status']
+#];
 
 // 2. Save to products.json
-$file = 'products.json';
-$current_data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
-$current_data[] = $product_data;
-file_put_contents($file, json_encode($current_data));
+#$file = 'products.json';
+#$current_data = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
+#$current_data[] = $product_data;
+#file_put_contents($file, json_encode($current_data));
 
 // 3. Send back a JSON response
-header('Content-Type: application/json');
-echo json_encode(['status' => 'success', 'message' => 'Product added successfully']);
+#header('Content-Type: application/json');
+#echo json_encode(['status' => 'success', 'message' => 'Product added successfully']);
+#?>
+
+
+<?php
+    $subject = $_POST['subject'];
+    $writer = $_POST['writer'];
+    $content = $_POST["content"];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+</head>
+<body>
+    <h3>입력하신 데이터는 다음과 같습니다.</h3>
+    <div>제목 : <?= $subject?> </div>
+</body>
+</html>
